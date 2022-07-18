6879c2c1b0e35bf102d161139623930da18df318
alexandreoliv <alexandrebo@gmail.com>
2022-07-19 00:22:52 +0200

    display player's name instead of ID

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
-// import "./App.css";
 import Table from "./Components/Table";
-import { Component } from "react";
 
 function App() {
-	const data = require("./api/stats/stats.json");
+	const stats = require("./api/stats/stats.json");
+	const players = require("./api/players.json");
 
 	return (
 		<div className="App">
 			<header className="App-header">
-				<div class="card text-center">
-					<h5 class="card-header">
+				<div className="card text-center">
+					<h5 className="card-header">
 						{"Basketball Stats by AOAO".toUpperCase()}
 					</h5>
-					<div class="card-body">
-						<h5 class="card-title">Who's the best player?</h5>
-						<p class="card-text">
+					<div className="card-body">
+						<h5 className="card-title">Who's the best player?</h5>
+						<p className="card-text">
 							Click on any column below to sort the table
 							ascending or descending
 						</p>
 						<a
 							href="https://github.com/aoaoberlin/basketball"
 							target="_blank"
-							class="btn btn-primary"
+							className="btn btn-primary"
 							rel="noreferrer"
 						>
 							Check us on GitHub
 						</a>
 					</div>
 				</div>
-				<Table data={data} />
+				<Table stats={stats} players={players} />
 			</header>
 		</div>
 	);
--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,15 +1,15 @@
 import React from "react";
 
 const Table = (props) => {
-	console.log(props);
-	const data = props.data;
+	const stats = props.stats;
+	const players = props.players.data;
 
 	return (
 		<div className="table-responsive-sm">
 			<table className="table table-hover table-striped">
 				<thead>
 					<tr>
-						<th scope="col">Player ID</th>
+						<th scope="col">Player</th>
 						<th scope="col">Season</th>
 						<th scope="col">Games played</th>
 						<th scope="col">Minutes played</th>
@@ -18,9 +18,15 @@ const Table = (props) => {
 					</tr>
 				</thead>
 				<tbody>
-					{data.map((player) => (
-						<tr key={player.player_id}>
-							<th scope="row">{player.player_id}</th>
+					{stats.map((player, index) => (
+						<tr key={String(index) + String(player.player_id)}>
+							<th scope="row">
+								{players
+									.filter((p) => p.id === player.player_id)
+									.map(
+										(p) => p.first_name + " " + p.last_name
+									)}
+							</th>
 							<td>{player.season}</td>
 							<td>{player.games_played}</td>
 							<td>{player.min}</td>
