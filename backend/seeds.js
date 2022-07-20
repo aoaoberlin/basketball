497714e213c15e89b7c6b8990d0378b488bbeae6
alexandreoliv <alexandrebo@gmail.com>
2022-07-21 00:54:22 +0200

    update paths to files

--- backend/seeds.js
+++ backend/seeds.js
@@ -1,47 +1,38 @@
 require("dotenv/config");
-
 const MONGO_URI = process.env.MONGODB_URI;
 
-const mongoose = require('mongoose');
-mongoose
-  .connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    // useFindAndModify: false,
-    // useCreateIndex: true
-  })
+const mongoose = require("mongoose");
+mongoose.connect(MONGO_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 
-const Season = require('./models/Season.model.js');
+const Season = require("./models/Season.model");
+const seasons = require("../frontend/src/data/stats/stats.json");
+const players = require("../frontend/src/data/players.json");
 
-const seasons = require('./src/api/stats/stats.json');
-
-const players = require('./src/api/players.json');
-
-const seasonsData = seasons.map(season => {
-    let properties = {
-        firstName: "",
-        lastName: "",
-        playerId: season.player_id,
-        year: season.season,
-        games: season.games_played,
-        points: season.pts,
-        rebounds: season.reb,
-        assists: season.ast,
-        steals: season.stl,
-        blocks: season.blk
-    }
-    console.log(properties);
-    let player = players.data.find(x => x.id === properties.playerId);
-    console.log(player);
-    properties.firstName = player.first_name;
-    properties.lastName = player.last_name;
-    console.log(properties);
-    return properties
+const seasonsData = seasons.map((season) => {
+	const properties = {
+		firstName: "",
+		lastName: "",
+		playerId: season.player_id,
+		year: season.season,
+		games: season.games_played,
+		points: season.pts,
+		rebounds: season.reb,
+		assists: season.ast,
+		steals: season.stl,
+		blocks: season.blk,
+	};
+	const player = players.data.find((x) => x.id === properties.playerId);
+	properties.firstName = player.first_name;
+	properties.lastName = player.last_name;
+	return properties;
 });
 
 Season.insertMany(seasonsData)
-    .then(seasons => {
-        console.log('Seasons added successfully: ', seasons);
-        mongoose.connection.close();
-    })
-    .catch(err => console.log(err));
+	.then((seasons) => {
+		console.log("Seasons added successfully");
+		mongoose.connection.close();
+	})
+	.catch((err) => console.log(err));
